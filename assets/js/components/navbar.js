fa82994225546fb3310a518daf63e5f24b25e56f
Farshid Ahmadi <farshidahmadi633@gmail.com>
2026-08-09 23:21:35 +0330

    Revert "Add responsive mobile navigation for English"
    This reverts commit 3a2ce6d6d0b74b991a685955e44aef9fb61e1bb2.

--- assets/js/components/navbar.js
+++ assets/js/components/navbar.js
@@ -1,96 +1,62 @@
 window.navbar = `
 
-<div class="container">
+<header class="navbar">
 
+    <div class="container">
 
-    <a href="/index.html"
-       class="logo">
-
-        MA
-
-    </a>
-
-
-    <button class="mobile-menu-toggle"
-            aria-label="Open menu"
-            aria-expanded="false">
-
-        ☰
-
-    </button>
-
-
-    <nav class="nav-menu">
-
-
-        <a href="/index.html">
-            Home
+        <a href="/index.html" class="logo">
+            MA
         </a>
 
+        <nav class="nav-menu">
 
-        <a href="/pages/about.html">
-            About
-        </a>
+            <a href="/index.html">
+                Home
+            </a>
 
+            <a href="/pages/about.html">
+                About
+            </a>
 
-        <a href="/pages/research.html">
-            Research
-        </a>
+            <a href="/pages/research.html">
+                Research
+            </a>
 
+            <a href="/pages/publications.html">
+                Publications
+            </a>
 
-        <a href="/pages/publications.html">
-            Publications
-        </a>
+            <a href="/pages/teaching.html">
+                Teaching
+            </a>
 
+           <a href="/pages/courses.html">
+                Courses
+            </a>
 
-        <a href="/pages/teaching.html">
-            Teaching
-        </a>
+            <a href="/pages/contact.html">
+                Contact
+            </a>
 
+        </nav>
 
-        <a href="/pages/courses.html">
-            Courses
-        </a>
+        <div class="nav-actions">
 
-
-        <a href="/pages/contact.html">
-            Contact
-        </a>
-
-
-        <a href="/fa/index.html"
-           class="mobile-language">
-
-            FA
-
-        </a>
-
-
-    </nav>
-
-
-    <div class="nav-actions">
+            <button
+                class="language-switch"
+                onclick="window.location.href='/fa/index.html'">
+                FA
+            </button>
 
 
-        <button
-            class="language-switch"
-            onclick="window.location.href='/fa/index.html'">
+            <button class="dark-mode-toggle">
+                ◐
+            </button>
 
-            FA
-
-        </button>
-
-
-        <button class="dark-mode-toggle">
-
-            ◐
-
-        </button>
-
+        </div>
 
     </div>
 
-
-</div>
+</header>
 
 `;
